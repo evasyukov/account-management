6411bb1884abb0dd97038844b8c017ce60b80e4a
evasyukov <egor.vasyukov1@gmail.com>
2024-03-14 13:17:44 +0700

    add layout and function addAccount

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Учетные записи</h1>
+  <button class="add-btn" @click="addAccount">+</button>
+  <AccountForm />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue"
+import AccountForm from "./components/AccountForm.vue"
+import { useAccountsStore } from "./store/accounts"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AccountForm,
+  },
+  setup() {
+    const accountsStore = useAccountsStore()
+
+    function addAccount() {
+      accountsStore.addAccount({
+        label: "",
+        type: "LDAP",
+        login: "",
+        password: null,
+      })
+    }
+
+    return {
+      addAccount,
+    }
+  },
+})
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+/* Стили для #app */
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 </style>
